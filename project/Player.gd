6b98834a66634c71c88cf79219ba0101751fcd86
avrhammer <avrhammer@deloitte.com.au>
2020-04-18 15:33:46 +1000

    load using standard godot

--- project/Player.gd
+++ project/Player.gd
@@ -9,7 +9,7 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-export var time = 0
+export var global_time = 0
 export var periods = 1.0
 export var speed = 1.13333333
 export var width = 20.0
@@ -17,10 +17,10 @@ export var heightMid = 150.0
 export var heightVar = 20.0
 
 func _process(delta):
-	time += delta
-	var height = get_height(time)
-	$head.set_position(Vector2(xposplayer(height,height), -height))
-	$head.set_rotation(head_angle(time))
+	global_time += delta
+	var height = get_height(global_time)
+	$head.set_position(Vector2(xposplayer(height,height, global_time), -height))
+	$head.set_rotation(head_angle(global_time))
 	update()
 
 func get_height (time):
@@ -30,10 +30,10 @@ func head_angle(time):
 	var height = get_height(time)
 	return atan(periods * 2 * PI / height * width * cos(periods * 2 * PI * (1 + time * speed)) )
 
-func xposplayer(h, height):
+func xposplayer(h, height, time):
 	return h / height * width * sin(periods * 2 * PI * (h / height + time * speed)) 
 
 func _draw():
-    var height = get_height(time)
-    for i in range(int(height)):
-        draw_line(Vector2(xposplayer(i, height),-i), Vector2(xposplayer(i+1, height), -(i+1)), ColorN("green"), 16)
+	var height = get_height(global_time)
+	for i in range(int(height)):
+		draw_line(Vector2(xposplayer(i, height, global_time),-i), Vector2(xposplayer(i+1, height,global_time), -(i+1)), ColorN("green"), 16)
