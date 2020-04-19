ac460d6a1e0d8add9e55db2c0b85aa9ec01255a7
avrhammer <avrhammer@deloitte.com.au>
2020-04-19 12:31:11 +1000

    centralize scripts in the src dir

---
+++ project/Src/ArrowController.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+var time = 0.0
+var lastEmit = 0.0
+export var PLAYER_GROUP_ID = ""
+export var emitsPerSecond = 1.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	time += delta
+	lastEmit += delta
+	if lastEmit > (1 / emitsPerSecond) :
+		lastEmit = 0.0
+		self.spawn_arrow()
+
+func spawn_arrow():
+	var child = load("res://Scenes/Arrow.tscn").instance()
+	var player_nodes = self.get_tree().get_nodes_in_group(PLAYER_GROUP_ID)
+	print(player_nodes)
+	if player_nodes:
+		child.set_player(player_nodes[0])
+		child.set_rotation(PI / 2 * (randi() % 4))
+		self.add_child(child)
+	else:
+		child.queue_free()
