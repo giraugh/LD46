43bd599e6f8238e9f4dec39fff009305341f3790
retroverse <ebreakey@gmail.com>
2020-04-19 14:07:34 +1000

    Add sun states and animation

---
+++ project/Src/Sun.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+# one of 0, 1, 2 (0 is sad, 1 is medium, 2 is happy)
+export(int, "Sad", "Ok", "Happy") var happy_state = 0
+var sun_animations = ["sad", "ok", "happy"]
+
+func _process(delta):
+	# Set animated sprites current animation from the happy_state ivar
+	$AnimatedSprite.animation = sun_animations[happy_state]
