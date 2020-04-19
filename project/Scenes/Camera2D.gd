cdfa1887bd0e00983393466df932628cf0f54999
Sam Nolan <samnolan555@gmail.com>
2020-04-19 16:44:45 +1000

    Parallax menu

--- project/Scenes/Camera2D.gd
+++ project/Scenes/Camera2D.gd
@@ -5,14 +5,23 @@ extends Camera2D
 # var a = 2
 # var b = "text"
 
-
+var background_original_scale = Vector2(0,0)
+var background_original_position = Vector2(221,587)
+var background_target_position = Vector2(221,587)
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	background_original_scale = get_parent().get_node("Scenery").get_scale()
 
-
+export var phase = 0
+export var background = "background-gardenl"
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 var global_time = 0
 func _process(delta):
 	global_time += delta
-	set_zoom(Vector2(1,1) * lerp(0.5,1, ((1 + sin(global_time)) / 2)))
+	phase = (1 + sin(global_time)) / 2
+	var zoom = lerp(4,1.3, phase)
+	var background = get_parent ().get_node("Scenery")
+	background.set_scale(background_original_scale * zoom / 1.3)
+	background.set_position(background_original_position.linear_interpolate(background_target_position, phase))
+	set_offset(Vector2(0,lerp(0,80, phase)))
+	set_zoom(Vector2(1,1) * zoom)
